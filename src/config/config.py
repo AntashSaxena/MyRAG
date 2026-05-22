5463a876c615e665be38de88651629335d14ac63
AntashSaxena <143865400+AntashSaxena@users.noreply.github.com>
2026-05-23 00:13:25 +0530

    Refactor OPENAI_API_KEY assignment method
    Updated OPENAI_API_KEY assignment to use environment variable.

--- src/config/config.py
+++ src/config/config.py
@@ -14,8 +14,8 @@ class Config:
     
     # API Keys
     #OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-    OPENAI_API_KEY = ${{ secrets.OPENAI_API_KEY }}
-    #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    #OPENAI_API_KEY = ${{ secrets.OPENAI_API_KEY }}
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     
     # Model Configuration
     LLM_MODEL = "openai:gpt-4o"
